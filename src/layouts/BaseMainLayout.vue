5e74c8617519b92fa2c1009550628a38bc869d9d
Knight <knight@priambodo.org>
2021-08-16 16:56:42 +0700

    Add Base Sidebar

---
+++ src/layouts/BaseMainLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+        <q-space/>
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round dense flat color="white" :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+                 @click="$q.fullscreen.toggle()"
+                 v-if="$q.screen.gt.sm">
+          </q-btn>
+          <q-btn round dense flat color="white" icon="fab fa-github" type="a" href="https://github.com/pratik227/quasar-admin" target="_blank">
+          </q-btn>
+          <q-btn round dense flat icon="fas fa-heart" style="color:#9d4182 !important;" type="a" href="https://github.com/sponsors/pratik227" target="_blank">
+          </q-btn>
+          <q-btn round dense flat color="white" icon="notifications">
+            <q-badge color="red" text-color="white" floating>
+              5
+            </q-badge>
+            <q-menu
+            >
+              <q-list style="min-width: 100px">
+                <messages></messages>
+                <q-card class="text-center no-shadow no-border">
+                  <q-btn label="View All" style="max-width: 120px !important;" flat dense
+                         class="text-indigo-8"></q-btn>
+                </q-card>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-primary text-white"
+    >
+      <base-sidebar>
+      </base-sidebar>
+    </q-drawer>
+
+    <q-page-container class="bg-grey-2">
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+    import EssentialLink from 'components/EssentialLink'
+    import BaseSidebar from 'components/sidebar/BaseSidebar'
+    import Messages from "./Messages";
+
+    export default {
+        name: 'MainLayout',
+
+        components: {
+            Messages,
+            EssentialLink,
+            BaseSidebar,
+        },
+
+        data() {
+            return {
+                leftDrawerOpen: false,
+            }
+        }
+    }
+</script>
