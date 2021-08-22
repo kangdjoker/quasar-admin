16afe3fb9d611b401106d5624d54dfe2edab94d5
Knight <knight@priambodo.org>
2021-08-22 20:53:29 +0700

    Report

---
+++ src/pages/report/ReportPermanentOut.vue
@@ -0,0 +1,15 @@
+<template>
+    <q-page class="q-pa-sm">
+    </q-page>
+</template>
+
+<script>
+
+export default {
+    name: 'ReportPermanentOut',
+    data() {
+        return {
+        }
+    }
+}
+</script>
