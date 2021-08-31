31b55d5ac475f0714bd75120a573ec02424deca5
Knight <knight@priambodo.org>
2021-09-01 06:47:56 +0700

    Logout

---
+++ src/pages/global/Logout.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+import AppUtils from "src/utils/AppUtils";
+
+export default {
+  name: "Logout",
+  mounted() {
+    AppUtils.clearUser()
+    this.$router.replace('/login')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
